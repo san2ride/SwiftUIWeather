f07e2253b68feed134df3f12b0e6f1072bdadd32
Jason <sanchezjason@me.com>
2024-02-28 14:18:53 -0700

    swiftui/currentWeather- GeocodingClient

--- CurrentWeather/CurrentWeather/Clients/APIEndpoint.swift
+++ CurrentWeather/CurrentWeather/Clients/APIEndpoint.swift
@@ -16,7 +16,15 @@ enum APIEndpoint {
     
     private var path: String {
         switch self {
-                
+            case .coordinatesByLocationName(let city):
+                return "/geo/1.0/direct?q=\(city)&appid=\(Constants.Keys.weatherAPIKey)"
+            case .weatherByLatLon(let lat, let lon):
+                return "/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(Constants.Keys.weatherAPIKey)"
         }
+    }
+    
+    static func endpointURL(for endpoint: APIEndpoint) -> URL {
+        let endpointPATH = endpoint.path
+        return URL(string: baseURL + endpointPATH)!
     }
 }
---
+++ CurrentWeather/CurrentWeather/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  CurrentWeather
+//
+//  Created by Cause  I'm Electric on 2/28/24.
+//
+
+import Foundation
+
+struct Constants {
+    struct Keys {
+        static let weatherAPIKey = "ba0d703a8332b5fa2f3a398e9c401321"
+    }
+}
